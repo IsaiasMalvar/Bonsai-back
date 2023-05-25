96901c30efcf968d0c1dc706fa18f16738d0c3a1
Isaías Malvar Guíos <isaiasmg93@hotmail.com>
2023-05-25 19:56:31 +0200

    Establish connection to database on server

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
 import "./loadEnvironment.js";
 import createDebug from "debug";
-import { app } from "./server/index.js";
 import chalk from "chalk";
+import connectToDatabase from "./database/connectToDatabase.js";
+import { app } from "./server/index.js";
 
 const debug = createDebug("bonsai-api:root");
 
 const port = process.env.PORT ?? 4000;
+const mongodbConnection = process.env.MONGODB_CONNECTION;
+
+if (!mongodbConnection) {
+  debug(chalk.red("Missing environment variables"));
+  process.exit(1);
+}
 
 app.listen(port, () => {
   debug(chalk.blue(`http://localhost:${port}`));
 });
+
+try {
+  await connectToDatabase(mongodbConnection);
+  debug(chalk.green("Connected to database"));
+} catch (error: unknown) {
+  debug(`Error connecting to database: ${(error as Error).message}`);
+}
