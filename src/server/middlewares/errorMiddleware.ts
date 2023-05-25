39c2949fcedf25b5563e1181b009377ca70dd98e
Isaías Malvar Guíos <isaiasmg93@hotmail.com>
2023-05-25 16:22:06 +0200

    Add generalError middleware

--- src/server/middlewares/errorMiddleware.ts
+++ src/server/middlewares/errorMiddleware.ts
@@ -1,5 +1,8 @@
+import createDebug from "debug";
 import { type NextFunction, type Request, type Response } from "express";
 import CustomError from "../../CustomError/CustomError";
+
+const debug = createDebug("bonsai-api:server:middlewares:errorMiddlewares");
 
 export const notFoundError = (
   _req: Request,
@@ -8,4 +11,20 @@ export const notFoundError = (
 ) => {
   const error = new CustomError(404, "Endpoint not found");
   next(error);
+};
+
+export const generalError = (
+  error: CustomError,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  debug(error.message);
+
+  const statusCode = error.statusCode || 500;
+  const message = error.statusCode
+    ? error.publicMessage
+    : "Internal Server Error";
+
+  res.status(statusCode).json({ message });
 };
