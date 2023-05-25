179a783456aa5a8fb6f0d2781f1913b5a3f66ae8
Isaías Malvar Guíos <isaiasmg93@hotmail.com>
2023-05-25 12:45:39 +0200

    Add allowed origin for cors

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,13 @@
+import cors from "cors";
 import "../loadEnvironment.js";
 import express from "express";
 
 export const app = express();
+
+const allowedOrigin = process.env.ALLOWED_ORIGIN;
+
+app.use(
+  cors({
+    origin: allowedOrigin,
+  })
+);
