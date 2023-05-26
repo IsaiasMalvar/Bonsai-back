be0669e6c23ce3d27aa9b8c560895a2adf8e7b39
Isaías Malvar Guíos <isaiasmg93@hotmail.com>
2023-05-26 20:13:31 +0200

    Add getMicrostories controller

---
+++ src/server/controllers/microstoriesControllers/microstoriesControllers.ts
@@ -0,0 +1,17 @@
+import { type NextFunction, type Request, type Response } from "express";
+import Microstory from "../../../database/models/Microstory";
+
+const getMicrostories = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const microstories = await Microstory.find().exec();
+    res.status(200).json({ microstories });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default getMicrostories;
