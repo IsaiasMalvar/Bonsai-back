37b6355a41170345b33a0c15c3808ecd26d35be4
Isaías Malvar Guíos <isaiasmg93@hotmail.com>
2023-06-10 14:53:38 +0200

    Add create endpoint to microsRouter

--- src/server/routers/microsRouter.ts
+++ src/server/routers/microsRouter.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
 import {
+  createMicro,
   deleteMicrostory,
   getMicrostories,
 } from "../controllers/microstoriesControllers/microstoriesControllers.js";
 import { auth } from "../middlewares/auth/authMiddleware.js";
+import paths from "../utils/paths/paths.js";
 
 const microsRouter = Router();
 
 microsRouter.get("/", auth, getMicrostories);
+microsRouter.post(paths.createController, auth, createMicro);
 microsRouter.delete("/:microId", auth, deleteMicrostory);
 
 export default microsRouter;
